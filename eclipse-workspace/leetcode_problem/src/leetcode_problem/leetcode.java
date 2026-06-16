ff764f0204de6a4d5c4bd7d1152feb37b655e539
sonali.rai <sonalirai29jan@gmail.com>
2026-06-16 14:59:34 +0530

    pattern problem

---
+++ eclipse-workspace/leetcode_problem/src/leetcode_problem/leetcode.java
@@ -0,0 +1,17 @@
+package leetcode_problem;
+
+public class leetcode {
+public static void pattern() {
+	
+	for (int i=1 ;i<5;i++) {
+	for (int j=1;j<5;j++) {
+		System.out.print("*");		
+	}	
+	System.out.println();	
+	}
+	
+}
+public static void main(String args[]) {
+	pattern();
+}
+}
